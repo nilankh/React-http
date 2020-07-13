715b72cefbda8882f592181824883d40489d8fbd
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-13 20:43:26 +0530

    http requests showed how error is working on ui

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -9,25 +9,27 @@ class PostList extends Component {
         super(props)
     
         this.state = {
-             posts: []
+             posts: [],
+             error: ''
         }
     }
     // third step use axios to make a get request to the Json placeholder api(https://jsonplaceholder.typicode.com/) the question is where do we place the code to make the get request,  the answer is in component did mount lifecycle method
 
     componentDidMount() {
         // as we know this method will be executed when the component mounts for the first time and this only executed once during components lifecycle a perfect place for our get request, to make a get request we invoke the get method on the axios library
-        axios.get('https://jsonplaceholder.typicode.com/posts')//this methods accepts the API endpoint as its arguments(now we are making a get request), but how do we access tje data that is returned, well axios is promised based library 
+        axios.get('https://jsonplaceholder.typicode.com/ggposts')//this methods accepts the API endpoint as its arguments(now we are making a get request), but how do we access tje data that is returned, well axios is promised based library 
             .then(response => {
                 console.log(response)
                 this.setState({posts: response.data})
             })
             .catch(error => {
                 console.log(error)
+                this.setState({errorMsg: 'Error retreiving data'})
             })
     }
 
     render() {
-        const { posts } = this.state
+        const { posts, errorMsg } = this.state
         return (
             <div>
                 List of Posts
@@ -36,6 +38,7 @@ class PostList extends Component {
                     posts.map(post => <div key={post.id}>{post.title}</div> ):
                     null
                 }
+                { errorMsg ? <div>{errorMsg}</div> : null}
             </div>
         )
     }
