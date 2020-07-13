d9baf2248c5dbf29af1624a2b29d3af38421b213
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-13 21:03:24 +0530

    learning about http post request lets create state properties for these three fields and linked them back to the input elements

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,18 +2,29 @@
 import React, { Component } from 'react'
 
 class PostForm extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             userId: '',
+             title: '',
+             body: ''
+        }
+    }
+    
     render() {
+        const { userId, title, body } = this.state
         return (
             <div>
                 <form>
                     <div>
-                        <input type="text" name="userId" />
+                        <input type="text" name="userId" value={userId} />
                     </div>
                     <div>
-                        <input type="text" name="title" />
+                        <input type="text" name="title" value={title} />
                     </div>
                     <div>
-                        <input type="text" name="body" />
+                        <input type="text" name="body" value={body} />
                     </div>
                 </form>
             </div>
