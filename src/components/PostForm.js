1cf48f972771b1d9ab2f08ca3e5375dba830b22a
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-13 21:08:40 +0530

    learning about http post request lets create state properties for these three fields and linked them back to the input elements and its working and showing in console now same we will do with axioms

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,35 +1,59 @@
 // HTTP POST REQUEST
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class PostForm extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             userId: '',
-             title: '',
-             body: ''
-        }
-    }
-    
-    render() {
-        const { userId, title, body } = this.state
-        return (
-            <div>
-                <form>
-                    <div>
-                        <input type="text" name="userId" value={userId} />
-                    </div>
-                    <div>
-                        <input type="text" name="title" value={title} />
-                    </div>
-                    <div>
-                        <input type="text" name="body" value={body} />
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userId: "",
+      title: "",
+      body: "",
+    };
+  }
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render() {
+    const { userId, title, body } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input
+              type="text"
+              name="userId"
+              value={userId}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="body"
+              value={body}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <button type="submit" >Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default PostForm
+export default PostForm;
