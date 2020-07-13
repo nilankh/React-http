92b464adca0c4acb88e2c0767fe1bf96c1e0651f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-13 20:57:43 +0530

    learning about http post request

---
+++ src/components/PostForm.js
@@ -0,0 +1,14 @@
+// HTTP POST REQUEST
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PostForm
