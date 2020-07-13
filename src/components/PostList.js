8a756e0f9f83d7ab3eb9bc0bb0d5bea606ae7fec
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-13 20:37:00 +0530

    http get request

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -16,7 +16,13 @@ class PostList extends Component {
 
     componentDidMount() {
         // as we know this method will be executed when the component mounts for the first time and this only executed once during components lifecycle a perfect place for our get request, to make a get request we invoke the get method on the axios library
-        axios.get('https://jsonplaceholder.typicode.com/posts')//this methods accepts the API endpoint as its arguments(now we are making a get request)
+        axios.get('https://jsonplaceholder.typicode.com/posts')//this methods accepts the API endpoint as its arguments(now we are making a get request), but how do we access tje data that is returned, well axios is promised based library 
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     render() {
